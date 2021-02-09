22ff82dcd5a87ff7c216375c96ad901268383a02
Igor <igor.lischewski@web.de>
2021-02-09 12:05:42 +0100

    02_IF-ELSE

--- script_01.js
+++ script_01.js
@@ -5,14 +5,15 @@ let ageJohn, ageMark;
 let isJohnOlder, isJohnEqual;
 
 // Wertzuweisung
-ageJohn = 25;
+ageJohn = 30;
 ageMark = 30;
 
 // Logische Aussagen / Test(s)
 isJohnOlder = (ageJohn > ageMark); // 30 > 30
 isJohnEqual = (ageJohn == ageMark); // 30 = 30
 
-
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
 console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
 console.log("---------------");
@@ -21,9 +22,24 @@ console.log("---------------");
 // alternativlos (TINA = There is no Alternative)
 
 //if (true)
+/*
 if (isJohnOlder)
 //if (ageJohn > ageMark)
 //if (55 > 30) 
 {
         console.log("John ist älter!");
+}
+*/
+
+
+/*************** IF - ELSE ***************/
+// mit Alternative
+
+if (isJohnOlder) 
+{ // Ja-Zweig | true
+    console.log("John ist älter!");
+} 
+else 
+{ // Nein-Zweig | false
+    console.log("John ist jünger!"); 
 }
